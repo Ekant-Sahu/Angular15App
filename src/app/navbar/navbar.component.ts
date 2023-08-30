b975018f20bc9a20b1212ecb601dc2d295b6c933
AmanDhananjay <112410719+AmanDhananjay@users.noreply.github.com>
2023-08-30 23:01:22 +0530

    nav scrool revil

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component,HostListener } from '@angular/core';
 import { AuthService } from '../service/auth.service';
 import { Router } from '@angular/router';
 import { ViewportScroller } from '@angular/common';
+import { VariableBinding } from '@angular/compiler';
 
 @Component({
   selector: 'app-navbar',
@@ -11,7 +12,14 @@ import { ViewportScroller } from '@angular/common';
 export class NavbarComponent {
   constructor(private services:AuthService,private router:Router,private viewportScroll:ViewportScroller){
    }
-
+   scrolledDown: boolean = false;
+   lastScrollTop: number = 0;
+   @HostListener('window:scroll', ['$event'])
+   onScroll(event: any) {
+       const st = window.pageYOffset || document.documentElement.scrollTop;
+       this.scrolledDown = st > this.lastScrollTop;
+       this.lastScrollTop = st <= 0 ? 0 : st;
+   }
 
    onClickScroll(elementId:string):void {
     this.viewportScroll.scrollToAnchor(elementId);
@@ -32,7 +40,7 @@ export class NavbarComponent {
       this.is_admin =false
     }
   }
-
+  
 
   
 
